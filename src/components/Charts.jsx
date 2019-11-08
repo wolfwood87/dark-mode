e9bc63e351e5b6a0921a340d7faea3ae3dfd1f3b
Derek <legato_5@yahoo.com>
2019-11-07 18:57:23 -0800

    added drop down menu for each coin.

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Chart from "./Chart";
 
 const Charts = ({ coinData }) => {
+  const [chart, setChart] = useState(false);
+
+  const changeChart = () => {
+    setChart(!chart)
+  }
   return (
     <div className="charts">
       {coinData.map(coin => (
@@ -12,7 +17,22 @@ const Charts = ({ coinData }) => {
             <img src={coin.image} height="40" alt={coin.name} />
           </div>
           <Chart sparklineData={coin.sparkline_in_7d.price} />
+          <p className={`${chart ? 'hide' : 'expand_button'}`} onClick={changeChart}>expand</p>
+          <div className={`${chart ? "coin_box" : 'hide'}`}>
+            <div className="coin_big_logo">
+              <img src={coin.image} alt={coin.name} />
+            </div>
+            <div className='coin_details'>
+              <h1>{coin.name}</h1>
+              <h2>{`Current Price: $${coin.current_price}`}</h2>
+              <h2>{`Market Cap Rank: ${coin.market_cap_rank}`}</h2>
+              <h2>{`Market Cap: $${coin.market_cap}`}</h2>
+              <h2>{`24h Low / 24h High $${coin.low_24h} / $${coin.high_24h} `}</h2>
+            </div>
+          </div>
+          <p className={`${chart ? 'expand_button' : 'hide'}`} onClick={changeChart}>collapse</p>
         </div>
+        
       ))}
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -15,7 +15,9 @@ const App = () => {
       .get(
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
-      .then(res => setCoinData(res.data))
+      .then(res => {
+        console.log(res)
+        setCoinData(res.data)})
       .catch(err => console.log(err));
   }, []);
   return (
