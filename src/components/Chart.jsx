bbc4e9e27cc21ed8d6a0a651965442d1d1961493
Derek <legato_5@yahoo.com>
2019-11-07 20:53:16 -0800

    able to get dropdown menus to work individually by moving them to Chart.jsx.

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
 import {
   LineChart,
@@ -9,8 +9,13 @@ import {
   Tooltip
 } from "recharts";
 
-const Chart = ({ sparklineData }) => {
-  const formattedData = sparklineData
+const Chart = (props) => {
+  const [chart, setChart] = useState(false);
+
+  const changeChart = () => {
+    setChart(!chart)
+  }
+  const formattedData = props.sparklineData
     .map((price, idx) => {
       if (idx % 6 === 0) {
         const timeToSubtract = 168 - idx;
@@ -18,7 +23,7 @@ const Chart = ({ sparklineData }) => {
           .subtract(timeToSubtract, "hours")
           .format("ddd h:mma");
         return { value: price, date };
-      } else if (idx === sparklineData.length - 1) {
+      } else if (idx === props.sparklineData.length - 1) {
         const date = moment().format("ddd h:mma");
         return { value: price, date };
       }
@@ -27,6 +32,7 @@ const Chart = ({ sparklineData }) => {
     .filter(data => data);
 
   return (
+    <div>
     <LineChart width={1100} height={300} data={formattedData}>
       <Line type="monotone" dataKey="value" stroke="#8884d8" />
       <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
@@ -34,6 +40,21 @@ const Chart = ({ sparklineData }) => {
       <YAxis />
       <Tooltip />
     </LineChart>
+    <p className={`${chart ? 'hide' : 'expand_button'}`} onClick={changeChart}>expand</p>
+    <div className={`${chart ? "coin_box" : 'hide'}`}>
+      <div className="coin_big_logo">
+        <img src={props.coin.image} alt={props.coin.name} />
+      </div>
+      <div className='coin_details'>
+        <h1>{props.coin.name}</h1>
+        <h2>{`Current Price: $${props.coin.current_price}`}</h2>
+        <h2>{`Market Cap Rank: ${props.coin.market_cap_rank}`}</h2>
+        <h2>{`Market Cap: $${props.coin.market_cap}`}</h2>
+        <h2>{`24h Low / 24h High $${props.coin.low_24h} / $${props.coin.high_24h} `}</h2>
+      </div>
+    </div>
+    <p className={`${chart ? 'expand_button' : 'hide'}`} onClick={changeChart}>collapse</p>
+  </div>
   );
 };
 
--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -9,28 +9,14 @@ const Charts = ({ coinData }) => {
   }
   return (
     <div className="charts">
-      {coinData.map(coin => (
+      {coinData.map((coin, index) => (
         <div className="chart__container" key={coin.name}>
           <h2 className="coin__title">{coin.name}</h2>
           <h4 className="coin__symbol">{coin.symbol}</h4>
           <div className="coin__logo">
             <img src={coin.image} height="40" alt={coin.name} />
           </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} />
-          <p className={`${chart ? 'hide' : 'expand_button'}`} onClick={changeChart}>expand</p>
-          <div className={`${chart ? "coin_box" : 'hide'}`}>
-            <div className="coin_big_logo">
-              <img src={coin.image} alt={coin.name} />
-            </div>
-            <div className='coin_details'>
-              <h1>{coin.name}</h1>
-              <h2>{`Current Price: $${coin.current_price}`}</h2>
-              <h2>{`Market Cap Rank: ${coin.market_cap_rank}`}</h2>
-              <h2>{`Market Cap: $${coin.market_cap}`}</h2>
-              <h2>{`24h Low / 24h High $${coin.low_24h} / $${coin.high_24h} `}</h2>
-            </div>
-          </div>
-          <p className={`${chart ? 'expand_button' : 'hide'}`} onClick={changeChart}>collapse</p>
+          <Chart sparklineData={coin.sparkline_in_7d.price} coin = {coin}/>
         </div>
         
       ))}
